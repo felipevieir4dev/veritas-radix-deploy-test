1235adea3484b7369cc2ca6d2117a6052209b604
Felipe Vieira <luis.f.santos40@aluno.senai.br>
2025-09-03 19:41:33 -0300

    Fix: Remove file logging to fix Render deployment

--- src/backend/veritas_radix/settings.py
+++ src/backend/veritas_radix/settings.py
@@ -231,12 +231,6 @@ LOGGING = {
         },
     },
     'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs' / 'django.log',
-            'formatter': 'verbose',
-        },
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
@@ -244,17 +238,12 @@ LOGGING = {
         },
     },
     'root': {
-        'handlers': ['console', 'file'],
+        'handlers': ['console'],
         'level': 'INFO',
     },
     'loggers': {
         'django': {
-            'handlers': ['console', 'file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'apps': {
-            'handlers': ['console', 'file'],
+            'handlers': ['console'],
             'level': 'INFO',
             'propagate': False,
         },
